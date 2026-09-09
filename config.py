b4548f46f2a583ff76d19bf71697a91e2ab90900
Varshit2448 <140231744+Varshit2448@users.noreply.github.com>
2026-09-09 11:47:14 +0530

    Update Readme

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "YOUR_PASSWORD")
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "plantfarma")
+    MYSQL_USER = os.getenv("MYSQL_USER", "avnadmin")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "AVNS_0FWlV26cWRk9UnHocgo")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "plantfarma-db-vorlaspn1981-bc35.g.aivencloud.com")
+    MYSQL_PORT = os.getenv("MYSQL_PORT", "27616")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "defaultdb")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
