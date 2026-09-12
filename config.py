647bf4e4a05c4c671c4ee9b12a73bb99627d5701
Varshit2448 <140231744+Varshit2448@users.noreply.github.com>
2026-09-12 13:17:17 +0530

    Refactor MySQL configuration to use environment variables

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    MYSQL_USER = os.getenv("MYSQL_USER", "avnadmin")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "AVNS_0FWlV26cWRk9UnHocgo")
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "plantfarma-db-vorlaspn1981-bc35.g.aivencloud.com")
-    MYSQL_PORT = os.getenv("MYSQL_PORT", "27616")
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "defaultdb")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = os.getenv("MYSQL_PORT")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
